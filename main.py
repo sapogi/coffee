477ed78895fa8566c82278c31018d5399e96efbe
Nikit <nikitagrigorov12@gmail.com>
2021-11-24 20:21:02 +0300

    работает

--- main.py
+++ main.py
@@ -7,6 +7,53 @@ from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
 from PyQt5.QtWidgets import QWidget, QTableView, QApplication, QMainWindow
 
 
+class addEditCoffeeForm(QWidget):
+    def __init__(self, *args):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.pushButton.clicked.connect(self.save)
+        self.pushButton_2.clicked.connect(self.cancel)
+
+    def save(self):
+        if self.lineEdit.text():
+
+            id = int(self.lineEdit.text())
+            sort = int(self.lineEdit_2.text())
+            roast = int(self.lineEdit_3.text())
+            form = self.lineEdit_4.text()
+            taste = self.lineEdit_5.text()
+            price = self.lineEdit_6.text()
+            size = int(self.lineEdit_7.text())
+            print(sort, roast, form, taste, price, size, id)
+            con = sqlite3.connect('coffee.sqlite')
+            cur = con.cursor()
+
+            res = cur.execute("""UPDATE coffee
+                                 SET (sort, roast, form, taste, price, size) = (?, ?, ?, ?, ?, ?)
+                                 WHERE id = ?""", (sort, roast, form, taste, price, size, id)).fetchall()
+            con.commit()
+            self.close()
+        else:
+            sort = int(self.lineEdit_2.text())
+            roast = int(self.lineEdit_3.text())
+            form = self.lineEdit_4.text()
+            taste = self.lineEdit_5.text()
+            price = self.lineEdit_6.text()
+            size = int(self.lineEdit_7.text())
+            con = sqlite3.connect('coffee.sqlite')
+            cur = con.cursor()
+            res = cur.execute("""INSERT INTO coffee(sort, roast, form, taste, price, size) 
+                                 VALUES(?, ?, ?, ?, ?, ?)""", (sort, roast, form, taste, price, size)).fetchall()
+            con.commit()
+            self.close()
+
+    def cancel(self):
+        self.close()
+
+
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -14,6 +61,8 @@ class Example(QMainWindow):
         self.initUI()
 
     def initUI(self):
+        self.pushButton.clicked.connect(self.run)
+        self.pushButton_2.clicked.connect(self.initUI)
         self.con = sqlite3.connect('coffee.sqlite')
         self.cur = self.con.cursor()
         res = self.cur.execute("""SELECT coffee.id, sorts.name, roast.title, coffee.form, coffee.taste, coffee.price, size.size  FROM coffee
@@ -26,7 +75,6 @@ class Example(QMainWindow):
         r = 1
         for row_number, row_data in enumerate(res):
             tableitem = []
-            model.insertRow(row_number)
             for value in row_data:
                 item = QStandardItem(str(value))
                 tableitem.append(item)
@@ -34,6 +82,11 @@ class Example(QMainWindow):
 
         self.view.setModel(model)
         self.setWindowTitle('кофе')
+
+    def run(self):
+        self.widget = addEditCoffeeForm(self)
+        self.widget.show()
+        self.update()
 
 
 if __name__ == '__main__':
